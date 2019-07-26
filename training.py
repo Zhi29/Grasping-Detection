25350a863727ed97cf9223ae7b1c02941659942a
Zhi Li <zhili@s-164-67-64-181.resnet.ucla.edu>
2019-07-26 11:43:58 -0700

    print

--- training.py
+++ training.py
@@ -46,7 +46,7 @@ def Loss_calculation(pred, label):
 
 def training():
     #temporarily use ResNet18 as our model
-    model = models.resnet50(pretrained = False)
+    model = models.resnet50(pretrained = True)
     num_ftrs = model.fc.in_features # the input dimension of fc of resnet18
     #model.fc = nn.Linear(num_ftrs, 5 * NUM_LABELS) # the output dim should be 5 corresponding to x, y, w, h, theta
     model.fc = nn.Sequential(nn.Linear(num_ftrs, 512), nn.Linear(512, 200), nn.Linear(200, 5*NUM_LABELS))
