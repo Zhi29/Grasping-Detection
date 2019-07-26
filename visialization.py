d3e0b1f40af688356488f4b424d92a1cc49b5882
Zhi Li <zhili@s-164-67-234-196.resnet.ucla.edu>
2019-07-25 20:00:51 -0700

    draw bbox

---
+++ visialization.py
@@ -0,0 +1,45 @@
+import cv2
+from training import *
+
+
+
+def process_label_files(label_path):
+    boxes = []
+    box = []
+    with open(label_path, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            a = line.split(';')
+            if float(a[2]) < 0:
+                # x y w h theta
+                box = [float(a[0]), float(a[1]), float(a[4]), float(a[3]), float(a[2])]
+            else:
+                # x y h w theta
+                box = [float(a[0]), float(a[1]), float(a[3]), float(a[4]), float(a[2])]
+            
+            boxes.append(box)
+    return boxes[:NUM_LABELS]
+
+def visulization():
+    imgs = glob.glob(dataset + "*" + ".png")
+    imgs.sort()
+    labels = glob.glob(dataset + "*" + ".txt")
+    labels.sort()
+
+    for i in range(len(imgs)):
+        img = imgs[i]
+        label = labels[i]
+
+        img = cv2.imread("{}.png".format(img))
+        cv2.nameWindow("Image" + str(i))
+
+        boxes = process_label_files(dataset + label)
+
+        for box in boxes:
+            bbox_contour = grasp_to_bbox(box)
+            cv2.drawContours(img, [bbox_contour], 0, (0, 0, 255), 1)
+
+        cv2.imshow('Image', img)
+        cv2.waitKey(0)
+
+visulization()
