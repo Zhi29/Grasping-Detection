4442286c51c28faab99288e66ba7e0f91f8cef0f
Zhi Li <zhili@s-164-67-64-181.resnet.ucla.edu>
2019-07-26 15:18:07 -0700

    cornell

--- grasping_evaluation.py
+++ grasping_evaluation.py
@@ -44,13 +44,11 @@ def acc(pred, label, num_imgs):
 
     for i in range(num_imgs):
         for j in range(label.size(1)):
-                pred_bbox = ((pred[i,j,0].detach().cpu().numpy(), pred[i,j,1].detach().cpu().numpy()), \
-                        (pred[i,j,2].detach().cpu().numpy(),pred[i,j,3].detach().cpu().numpy()),\
-                        pred[i,j,4].detach().cpu().numpy())
-                pred_bbox = cv2.boxPoints(pred_bbox)
+            pred_bbox = ((pred[i,j,0].detach().cpu().numpy(), pred[i,j,1].detach().cpu().numpy()), \
+                    (pred[i,j,2].detach().cpu().numpy(),pred[i,j,3].detach().cpu().numpy()),\
+                    pred[i,j,4].detach().cpu().numpy())
+            pred_bbox = cv2.boxPoints(pred_bbox)
             for k in range(label.size(1)):
-                #pred_bbox = grasp_to_bbox(pred[i,j,:].detach().cpu().numpy())
-                #label_bbox = grasp_to_bbox(label[i,j,:].detach().cpu().numpy())
                 label_bbox = ((label[i,k,0].detach().cpu().numpy(), label[i,k,1].detach().cpu().numpy()), \
                         (label[i,k,2].detach().cpu().numpy(),label[i,k,3].detach().cpu().numpy()),\
                         label[i,k,4].detach().cpu().numpy())
