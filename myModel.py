aecd608d7347d7550e199867dd54eb6ffd4b35d9
Zhi Li <zhili@s-164-67-64-18.resnet.ucla.edu>
2019-07-25 22:27:23 -0700

    model

--- myModel.py
+++ myModel.py
@@ -14,7 +14,7 @@ class myModel(nn.Module):
         self.Conv6 = nn.Sequential(nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #9
 
         self.fc1 = nn.Linear(8*8*256, 512)
-        self.activate = nn.ReLU()
+        self.activate = nn.Sigmoid()
         self.dropout = nn.Dropout(p = dropout_rate)
         self.fc2 = nn.Linear(512, 512)
         self.out = nn.Linear(512, NUM_LABELS*5)
@@ -31,9 +31,7 @@ class myModel(nn.Module):
 
         x = x.view(x.size(0), -1)
         x = self.activate(self.fc1(x))
-        x = self.dropout(x)
         x = self.activate(self.fc2(x))
-        x = self.dropout(x)
         output = self.out(x)
 
         return output
