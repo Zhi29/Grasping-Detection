71c999f3fd7157aa787a1ca4a61ddbb3a7ebb4b2
Zhi Li <zhili@s-164-67-64-18.resnet.ucla.edu>
2019-07-25 22:44:47 -0700

    model

--- training.py
+++ training.py
@@ -24,7 +24,7 @@ writer = SummaryWriter(log_dir = 'log')
 DATA_SPLIT = 0.9 # the split of training and validation data
 EPOCH = 200
 BATCH_SIZE = 8
-lr = 0.00005
+lr = 0.0001
 GPU = True
 
 train_data = MyDataset(dataset = dataset, start = 0, end = DATA_SPLIT, transform = transforms.Compose([transforms.Resize(640), transforms.ToTensor(), transforms.Normalize(mean, std)]))
