2530e789ac880daa58aff6bb608d56f864d7e41d
Zhi Li <zhili@s-164-67-64-181.resnet.ucla.edu>
2019-07-26 13:35:01 -0700

    cornell

--- dataprocess.py
+++ dataprocess.py
@@ -100,7 +100,7 @@ class MyDataset_Cornell(torch.utils.data.Dataset):
     
 
 def test():
-    traindata = MyDataset_Cornell(dataset = dataset, start = 0, end = 0.8, transform = transforms.Compose([transforms.Resize(640), transforms.ToTensor()]))
+    traindata = MyDataset_Cornell(dataset = dataset, start = 0, end = 0.8, transform = transforms.Compose([transforms.ToTensor()]))
     trainloader = torch.utils.data.DataLoader(dataset = traindata, batch_size=2, shuffle=False)
     for i, data in enumerate(trainloader):
         imags, label = data
