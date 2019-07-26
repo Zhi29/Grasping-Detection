ae0c28fc79a125ae0fde3491a6161e6d9897e30a
Zhi Li <zhili@s-164-67-234-196.resnet.ucla.edu>
2019-07-25 20:15:32 -0700

    draw bbox

--- visialization.py
+++ visialization.py
@@ -38,8 +38,7 @@ def visulization():
         for box in boxes:
             bbox_contour = grasp_to_bbox(box)
             print(bbox_contour)
-            bbox_contour = bbox_contour.tolist()
-            cv2.drawContours(img, [bbox_contour], 0, (0, 255, 0), 1)
+            cv2.drawContours(img, bbox_contour, 0, (0, 255, 0), 1)
 
         cv2.imshow('Image', img)
         cv2.waitKey(0)
