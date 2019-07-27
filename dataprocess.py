d10065b0144b94b6fa3e91a8959c0188e68da1c9
Zhi Li <zhili@s-164-67-64-236.resnet.ucla.edu>
2019-07-26 17:14:48 -0700

    cornell

--- dataprocess.py
+++ dataprocess.py
@@ -6,9 +6,9 @@ import torch.utils.data as Dataset
 import torchvision.transforms as transforms
 
 from PIL import Image
-#dataset = "/home/yunchu/python_workspace/test10/" # this is the path of dataset
+dataset = "/home/yunchu/python_workspace/test10/" # this is the path of dataset
 #dataset = "/home/yunchu/Workspace/Deep_CNN_with_VAE_for_graspe/test10cornell/"
-dataset = "/Users/zhili/Documents/test_dataset/"
+#dataset = "/Users/zhili/Documents/test_dataset/"
 
 NUM_LABELS = 10
 mean = [0.485, 0.456, 0.406],
