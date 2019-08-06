ed9dddf38360ea6d95aa45ef12ed8a1b0b138f53
Zhi Li <zhili@s-149-142-48-57.resnet.ucla.edu>
2019-08-06 14:09:11 -0700

    theta

--- myModel.py
+++ myModel.py
@@ -6,12 +6,12 @@ dropout_rate = 0.2
 class myModel(nn.Module):
     def __init__(self):
         super(myModel, self).__init__()
-        self.Conv1 = nn.Sequential(nn.Conv2d(in_channels = 3 , out_channels = 32, kernel_size = 5, stride = 1, padding = 2), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #320
-        self.Conv2 = nn.Sequential(nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 5, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #158
-        self.Conv3 = nn.Sequential(nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #78
-        self.Conv4 = nn.Sequential(nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #38
-        self.Conv5 = nn.Sequential(nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 3, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #18
-        self.Conv6 = nn.Sequential(nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1), nn.ReLU(), nn.MaxPool2d(kernel_size = 2)) #9
+        self.Conv1 = nn.Sequential(nn.Conv2d(in_channels = 3 , out_channels = 32, kernel_size = 5, stride = 1, padding = 2), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #320
+        self.Conv2 = nn.Sequential(nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 5, stride = 1), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #158
+        self.Conv3 = nn.Sequential(nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, stride = 1), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #78
+        self.Conv4 = nn.Sequential(nn.Conv2d(in_channels = 128, out_channels = 128, kernel_size = 3, stride = 1), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #38
+        self.Conv5 = nn.Sequential(nn.Conv2d(in_channels = 128, out_channels = 256, kernel_size = 3, stride = 1), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #18
+        self.Conv6 = nn.Sequential(nn.Conv2d(in_channels = 256, out_channels = 256, kernel_size = 3, stride = 1), nn.LeakyReLU(), nn.MaxPool2d(kernel_size = 2)) #9
 
         self.fc1 = nn.Linear(8*8*256, 1024)
         self.activate = nn.ReLU()
